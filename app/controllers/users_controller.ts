bab81611bd4334fe7787da2478d229edf2040edf
Bruno Mendes <Bruno.mendes2804@gmail.com>
2024-07-15 18:51:33 -0300

    style: remove unused variables

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -3,7 +3,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import hash from '@adonisjs/core/services/hash'
 
 export default class UsersController {
-  async store({ request, response, auth }: HttpContext) {
+  async store({ request, response }: HttpContext) {
     const user = await User.create({
       ...request.body(),
       password: await hash.make(request.body().password),
