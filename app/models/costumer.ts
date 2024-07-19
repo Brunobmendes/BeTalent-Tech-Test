43a5c7608d1cce1862727c8ae35ecffdd12c72d3
Bruno Mendes <Bruno.mendes2804@gmail.com>
2024-07-18 23:04:20 -0300

    feat: sales model

--- app/models/costumer.ts
+++ app/models/costumer.ts
@@ -3,6 +3,7 @@ import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import Phone from './phone.js'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
 import Address from './address.js'
+import Sale from './sale.js'
 
 export default class Costumer extends BaseModel {
   @column({ isPrimary: true })
@@ -18,6 +19,8 @@ export default class Costumer extends BaseModel {
   declare Phones: HasMany<typeof Phone>
   @hasMany(() => Address)
   declare Addresses: HasMany<typeof Address>
+  @hasMany(() => Sale)
+  declare Sales: HasMany<typeof Sale>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
---
+++ app/models/sale.ts
@@ -0,0 +1,46 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeSave, belongsTo, column } from '@adonisjs/lucid/orm'
+import Costumer from './costumer.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Product from './product.js'
+import { SaleStatus } from './enums/sale_enum.js'
+
+export default class Sale extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare costumerId: number
+  @column()
+  declare productId: number
+  @column()
+  declare quantity: string
+  @column()
+  declare unitPrice: number
+  @column()
+  declare totalPrice: number
+  @column()
+  declare status: SaleStatus
+
+  @belongsTo(() => Costumer)
+  declare costumer: BelongsTo<typeof Costumer>
+
+  @belongsTo(() => Product)
+  declare product: BelongsTo<typeof Product>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: false, autoUpdate: false })
+  declare finishedAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @beforeSave()
+  static async setFinishedAt(sale: Sale) {
+    if (sale.$dirty.status && sale.status === 'finished') {
+      sale.finishedAt = DateTime.now()
+    }
+  }
+}
